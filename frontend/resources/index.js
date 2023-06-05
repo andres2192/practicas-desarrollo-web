a76680c473d42d81d85a8ba2ee903f69c277d75b
andres2192 <117128398+andres2192@users.noreply.github.com>
2023-06-05 20:33:53 -0300

    practicas aplicacion web - frontend - agregando css y otras funcionalidades

--- frontend/resources/index.js
+++ frontend/resources/index.js
@@ -4,6 +4,9 @@ const form = document.getElementById("transactionForm");
 //le colocamos una escucha de eventos, que escuche el evento "submit" del
 //boton del formulario, para hacer otras funciones con ello
 form.addEventListener("submit", function(event) {
+
+    //aca se deberian agregar validaciones para el evento submit
+
     // 1- prevenimos (por el momento) el funcionamiento por defecto del evento 
     //submit que envia los datos al servidor y recarga automaticamente la pagina
     event.preventDefault();
@@ -32,6 +35,9 @@ form.addEventListener("submit", function(event) {
 //guardados en el localStorage al terminar de cargarse el DOM (primero se termina de
 //cargar el DOM luego sigue con el CSS, los scripts y demas cosas)
 document.addEventListener("DOMContentLoaded", function(event) {
+    //primero corremos la funcion para cargar todas las categorias que tengamos creadas
+    drawCategory();
+    
     //traemos lo guardado en el localStorage previamente convertido desde el formato json
     let transactionObjArr = JSON.parse(localStorage.getItem("transactionData")) || [];
 
@@ -74,12 +80,18 @@ function convertFormDataToTransactionObject(transactionFormData) {
 };
 
 //funcion para insertar nuevas filas en la tabla del archivo html
+//agregamos boton eliminar al crear nuava fila con un eventListener para eliminar datos
+//del local storage
 function insertRowInTransactionTable(transactionObjet) {
     //hacemos una referencia a la tabla mediante su id en el html
     let transactionTableRef = document.getElementById("transactionTable");
 
     //.insertRow(): metodo nativo para insertar una fila en la tabla
     let newTransactionRowRef = transactionTableRef.insertRow(-1);
+
+    //al insertar la nueva fila, le añadimos un id mediante un atributo personalizado
+    //para saber cual seleccionar al momento de eliminar cualquier dato
+    newTransactionRowRef.setAttribute("data-transaction-id", transactionObjet["transactionId"]);
 
     //aca pasamos los valores que tendran cada celda de la nuava fila agregada
     //mediante el metodo .incertCell() para seleccionar la celda afectada, y la
@@ -103,12 +115,22 @@ function insertRowInTransactionTable(transactionObjet) {
     //agregamos un boton para eliminar la fila correspondiente
     let newDeleteCell = newTransactionRowRef.insertCell(4);
     let deletebutton = document.createElement("button");
-    deletebutton.textContent = "eliminar";
+    deletebutton.textContent = "Eliminar";
     newDeleteCell.appendChild(deletebutton);
 
-    //agregamos un ventListener al boton eliminar para que ejecute la funcion de eliminar
+    //agregamos un eventListener al boton eliminar para que ejecute la funcion de eliminar
+    //elimina la fila y los datos del localStorage
     deletebutton.addEventListener("click", (event) =>{
-        event.target.parentNode.parentNode.remove();
+        //al hacer click, primero selecciona la fila de la tabla a eliminar
+        let transactionRow = event.target.parentNode.parentNode;
+        //luego selecciona el id especificado para esa fila mediante el atributo
+        //data-transaction-id
+        let transactionId = transactionRow.getAttribute("data-transaction-id");
+        //eliminamos la fila seleccionada en transactionRow
+        transactionRow.remove();
+        //eliminamos los datos correspondientes en el localStorage mediante la funcion
+        //deleteTransactionObject()
+        deleteTransactionObject(transactionId);
     });
 };
 
@@ -130,4 +152,42 @@ function saveTransactionObjet(transactionObjet) {
     //funcion nativa para guardar el json en la memoria local. la funcion tiene dos
     //parametros: el nombre que sera la clave para referencia, y el valor a guardar
     localStorage.setItem("transactionData", transactionArrayJSON);
+};
+
+//funcion para eliminar objetos del localStorage
+function deleteTransactionObject(transactionId){
+    //tomamos el array desed el localStorage y lo convertimos de formato json a objeto javascript
+    let transactionObjArr = JSON.parse(localStorage.getItem("transactionData"));
+    //filtramos el elemento dentro del array con el id que queremos eliminar
+    //aca habia un error en la comparacion ya que tomaba el valor guardado en el localStorage que
+    //era un entero y lo comparaba estrictamente con un string
+    let transactionIndexInArray = transactionObjArr.findIndex(element => element.transactionId == transactionId);
+    //eliminamos el indice del elemento que contenga el id seleccionado en el array original
+    transactionObjArr.splice(transactionIndexInArray, 1);
+    //el array que se devuelve con el elemento ya eliminado lo convertimos a json nuevamente
+    //y lo guardamos en el localStorage
+    let transactionArrayJSON = JSON.stringify(transactionObjArr);
+    localStorage.setItem("transactionData", transactionArrayJSON);
+};
+
+//las dos funciones para agregar categorias solo agregan las categorias que ya estan
+//cargadas en el array de la funcion drawCategory, entonces faltaria algo para que el
+//usuario ingresara nuavas categorias y estas se guardaran en el array
+
+//funcion para crear las categorias a agregar
+function drawCategory() {
+    let allCategories = ["Alquiler", "Comida", "Diversion"];
+    for(let index = 0; index < allCategories.length; index++) {
+        insertCategory(allCategories[index]);
+    }
+};
+
+//funcion para agregar nuevas categorias al formulario
+function insertCategory(categoryName) {
+    //seleccionamos el elemento donde insertar la nueva categoria
+    const selectElement = document.getElementById("transactionCategory");
+    //pasamos en una variable el template string a insertar
+    let htmlToInsert = `<option>${categoryName}</option>`;
+    //con insertAdjacentHTML pasamos la posicion y el string a insertar
+    selectElement.insertAdjacentHTML("beforeend", htmlToInsert)
 };
